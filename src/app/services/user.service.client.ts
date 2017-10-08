ea97a904e76c0d7fa186304e7f0bc85f41582f47
mhanna14151 <31774802+mhanna14151@users.noreply.github.com>
2017-10-08 18:17:01 -0400

    added the website.service.client.ts CRUD operations.

--- src/app/services/user.service.client.ts
+++ src/app/services/user.service.client.ts
@@ -12,15 +12,18 @@ export class UserService {
   constructor() { }
 
   users = [
-    {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
-    {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
-    {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
-    {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
+    {_id: '123', username: 'alice',    password: 'alice',    firstName: 'Alice',  lastName: 'Wonder'  },
+    {_id: '234', username: 'bob',      password: 'bob',      firstName: 'Bob',    lastName: 'Marley'  },
+    {_id: '345', username: 'charly',   password: 'charly',   firstName: 'Charly', lastName: 'Garcia'  },
+    {_id: '456', username: 'jannunzi', password: 'jannunzi', firstName: 'Jose',   lastName: 'Annunzi' }
   ];
 
   api = {
     'createUser'   : this.createUser,
-    'findUserById' : this.findUserById
+    'findUserById' : this.findUserById,
+    'findUserByUsername' : this.findUserByUsername,
+    'updateUser' : this.updateUser,
+    'deleteUser' : this.deleteUser
   };
 
   createUser(user: any) {
---
+++ src/app/services/website.service.client.ts
@@ -0,0 +1,72 @@
+import { Injectable } from '@angular/core';
+import { Http, RequestOptions, Response } from '@angular/http';
+import 'rxjs/Rx';
+import { environment } from '../../environments/environment';
+import { Router } from '@angular/router';
+
+// injecting service into module
+@Injectable()
+
+export class WebsiteService {
+
+  constructor() { }
+
+  websites = [
+    { '_id': '123', 'name': 'Facebook',    'developerId': '456', 'description': 'Lorem' },
+    { '_id': '234', 'name': 'Tweeter',     'developerId': '456', 'description': 'Lorem' },
+    { '_id': '456', 'name': 'Gizmodo',     'developerId': '456', 'description': 'Lorem' },
+    { '_id': '890', 'name': 'Go',          'developerId': '123', 'description': 'Lorem' },
+    { '_id': '567', 'name': 'Tic Tac Toe', 'developerId': '123', 'description': 'Lorem' },
+    { '_id': '678', 'name': 'Checkers',    'developerId': '123', 'description': 'Lorem' },
+    { '_id': '789', 'name': 'Chess',       'developerId': '234', 'description': 'Lorem' }
+  ]
+  ;
+
+  api = {
+    'createWebsite'   : this.createWebsite,
+    'findWebsitesByUser' : this.findWebsitesByUser,
+    'findWebsiteById' : this.findWebsiteById,
+    'updateWebsite' : this.updateWebsite,
+    'deleteWebsite' : this.deleteWebsite
+  };
+
+  // adds the website parameter instance to the local websites array.
+  // The new website's developerId is set to the userId parameter
+  createWebsite(userId, website) {
+    website.developerId = userId;
+    this.websites.push(website);
+    return website;
+  }
+  // retrieves the websites in local websites array whose developerId matches the parameter userId
+  findWebsitesByUser(userId) {
+    for (let x = 0; x < this.websites.length; x++) {
+      if (this.websites[x].developerId === userId) {
+        return this.websites[x]; }
+    }
+  }
+  // retrieves the website in local websites array whose _id matches the websiteId parameter
+  findWebsiteById(websiteId) {
+    for (let x = 0; x < this.websites.length; x++) {
+      if (this.websites[x]._id === websiteId) {
+        return this.websites[x];
+      }
+    }
+  }
+  // updates the website in local websites array whose _id matches the websiteId parameter
+  // may have to confirm this works
+  updateWebsite(websiteId, website) {
+    for (let x = 0; x < this.websites.length; x++) {
+      if (this.websites[x]._id === websiteId) {
+        this.websites[x] = website;
+      }
+    }
+  }
+  // removes the website from local websites array whose _id matches the websiteId parameter
+  deleteWebsite(websiteId) {
+    for (let x = 0; x < this.websites.length; x++) {
+      if (this.websites[x]._id === websiteId) {
+        delete this.websites[x];
+      }
+    }
+  }
+}
