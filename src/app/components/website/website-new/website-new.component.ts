a76b2180d92118806acca9ea69fe0ebaebb8a1e0
mhanna14151 <31774802+mhanna14151@users.noreply.github.com>
2017-10-10 18:58:11 -0400

    formatted the website-new and website-edit html documents to project a more elegant view.

--- src/app/components/website/website-new/website-new.component.ts
+++ src/app/components/website/website-new/website-new.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {Website} from "../../../models/website.model.client";
+import {UserService} from "../../../services/user.service.client";
+import {WebsiteService} from "../../../services/website.service.client";
+import {PageService} from "../../../services/page.service.client";
+import {ActivatedRoute, Router} from "@angular/router";
+import {User} from "../../../models/user.model.client";
 
 @Component({
   selector: 'app-website-new',
@@ -6,10 +12,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./website-new.component.css']
 })
 export class WebsiteNewComponent implements OnInit {
+  userId: String;
+  user: User;
+  name: String;
+  description: String;
+  website: Website;
+  websiteId: String;
 
-  constructor() { }
+  // websites: Website[];
 
-  ngOnInit() {
+  constructor(private userService: UserService,
+              private websiteService: WebsiteService,
+              private pageService: PageService,
+              private route: ActivatedRoute,
+              private router: Router) {
   }
 
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.userId = params['uid'];
+      this.websiteId = params['wid']
+      this.website = this.websiteService.findWebsiteById(this.websiteId);
+      this.user = this.userService.findUserById(this.userId);
+      // this.name = this.website.name;
+      // this.description = this.website.description;
+    });
+  }
+
+  outputWebsitesForThisUser() {
+    const websiteList: Website[] = this.websiteService.findWebsitesByUser(this.userId);
+    // console.log(this.websiteService.findWebsitesByUser(this.userId));
+    return websiteList;
+  }
+
+  navigateToWebsiteEdit(ID) {
+    this.router.navigate(['user/', this.user._id, 'website', ID]);
+  }
+
+  // updateWebsite(ID, name: String, description: String) {
+  //   this.websiteService.updateWebsite(ID, new Website(ID, name, this.userId, description));
+  // }
+
+  navigateToUsersWebsite() {
+    this.router.navigate(['user', this.userId, 'website']);
+  }
+
+  navigateToWebsiteNew() {
+    this.router.navigate(['user/', this.user._id, 'website', 'new']);
+  }
+
+  returnToProfile() {
+    this.router.navigate(['user/', this.user._id]);
+  }
 }
