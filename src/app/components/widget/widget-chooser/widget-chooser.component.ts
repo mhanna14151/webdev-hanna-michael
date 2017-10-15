417da4ccf37bd9494c4e04fbbdefe9f109a51138
mhanna14151 <31774802+mhanna14151@users.noreply.github.com>
2017-10-15 15:38:22 -0400

    Seemingly stable version. I believe I have added all the assignment requirements. Next phase, testing and cleaning of code.

--- src/app/components/widget/widget-chooser/widget-chooser.component.ts
+++ src/app/components/widget/widget-chooser/widget-chooser.component.ts
@@ -1,14 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import {User} from "../../../models/user.model.client";
-import {Website} from "../../../models/website.model.client";
-import {Page} from "../../../models/page.model.client";
-import {Widget} from "../../../models/widget.model.client";
-import {UserService} from "../../../services/user.service.client";
-import {WebsiteService} from "../../../services/website.service.client";
-import {PageService} from "../../../services/page.service.client";
-import {WidgetService} from "../../../services/widget.service.client";
-import {ActivatedRoute, Router} from "@angular/router";
-import {DomSanitizer} from "@angular/platform-browser";
+import {User} from '../../../models/user.model.client';
+import {Website} from '../../../models/website.model.client';
+import {Page} from '../../../models/page.model.client';
+import {Widget} from '../../../models/widget.model.client';
+import {WidgetService} from '../../../services/widget.service.client';
+import {ActivatedRoute, Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-widget-chooser',
@@ -24,18 +21,14 @@ export class WidgetChooserComponent implements OnInit {
   pages: Page[];
   widget: Widget;
   pageID: String;
-  widgets: Widget[];
   widgetID: String;
   url: string;
 
 
-  constructor(private userService: UserService,
-              private websiteService: WebsiteService,
-              private pageService: PageService,
+  constructor(
               private widgetService: WidgetService,
               private route: ActivatedRoute,
-              private router: Router,
-              private sanitzer: DomSanitizer) { }
+              private router: Router) { }
 
 
   ngOnInit() {
@@ -47,13 +40,11 @@ export class WidgetChooserComponent implements OnInit {
   }
 
   returnToProfile() {
-    this.router.navigate(['user/', this.user._id]);
+    this.router.navigate(['user/', this.userId]);
   }
 
-// (_id, widgetType, pageId, size, width, text, url) {
   createNewWidget(type: String) {
     this.widgetType = type;
-    // const newWidget: Widget = new Widget(null, this.widgetType, this.pageID, null, null, null, null);
     this.widget = this.widgetService.createWidget(this.pageID,
       new Widget(null, this.widgetType, this.pageID, null, null, null, null));
     console.log('ID' + this.widget._id);
@@ -63,20 +54,8 @@ export class WidgetChooserComponent implements OnInit {
     this.widgetID]);
   }
 
-  dyanamicWidth(width) {
-    return width.toString;
-
-  }
-
-  navigateToWidgetChooser() {
-    this.router.navigate(['WidgetChooserComponent']);
-  }
-
-  cleanThisUrl(url) {
-    let thisUrl = 'https://www.youtube.com/embed/';
-    const end = url.split('/');
-    thisUrl += end[end.length - 1];
-    return this.sanitzer.bypassSecurityTrustResourceUrl(thisUrl);
+  returnToPreviousSite() {
+    this.router.navigate(['user/', this.userId, 'website', this.websiteId, 'page', this.pageID, 'widget']);
   }
 
 }
